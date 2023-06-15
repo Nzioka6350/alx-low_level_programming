d93d40688a21d5b320c47175b6ccfaede2970cf6
Nzioka6350 <Nzioka6350>
2023-06-15 03:00:04 -0700

    M

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(Void)
+{
+/* Main -Start Point
+ * Return -returns 0 unless otherwise
+ */
+printf("with proper grammar, but the outcome is a piece of art,");
+Return (0);
+}
+
