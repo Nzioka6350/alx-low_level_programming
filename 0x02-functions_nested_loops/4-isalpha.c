a9488cf2a8eb26ff55d9b3e9015fedcb9716273e
Nzioka6350 <Nzioka6350>
2023-06-20 04:02:36 -0700

    Correct

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /*
- *int _isalpha - checks for alphabetic character
- */
+*int _isalpha - checks for alphabetic character
+*/
 int _isalpha(int c)
 {
 return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
