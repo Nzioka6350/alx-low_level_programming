77b48690f7ca4d06dd5f015edb9c6569610a954b
Nzioka6350 <Nzioka6350>
2023-06-20 05:51:20 -0700

    M

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,19 +6,19 @@
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
 */
-int print_sign(int n)
+int print_sign(int n)xbaxb
+
+ (n > 0)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	} else if (n == 0)
+_putchar('+');
+return (1);
+}else if (n == 0)
 }
-		_putchar(48);
-		return (0);
-	} else if (n < 0)
-	{
-		_putchar('-');
-	}
-		return (-1);
+_putchar(48);
+return (0);
+} else if (n < 0)
+{
+_putchar('-');
+}
+return (-1);
 }
