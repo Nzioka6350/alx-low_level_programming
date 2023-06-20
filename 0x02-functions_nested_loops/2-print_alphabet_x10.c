316c7ba880f1204d45186f85544065cc6b42466e
Nzioka6350 <Nzioka6350>
2023-06-20 03:18:48 -0700

    Main

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,6 @@
 #include "main.h"
-/*
- *main - check the code.
- *Return: Always 0.
- * print_alphabet_x10 - prints alphabets 10times in lowercase
+/**
+ * print_alphabet - print all alphabet in lowercase
  */
 void print_alphabet_x10(void)
 {
