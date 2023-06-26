bcf9a4768c2266e0cbee706360195d991aaec92c
Nzioka6350 <Nzioka6350>
2023-06-26 04:00:59 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+     }
