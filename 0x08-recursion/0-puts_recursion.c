0fad32084ec373a18320bafdaa1a918e3ade5811
Nzioka6350 <Nzioka6350>
2023-07-05 01:04:00 -0700

    0x08. C - Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
-/**
- * _puts_recursion - function like puts();
- * @s: input
- * Return: Always 0 (Success)
- */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
 void _puts_recursion(char *s)
 {
 	if (*s)
@@ -11,7 +12,6 @@ void _puts_recursion(char *s)
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-
 	else
 		_putchar('\n');
 }
