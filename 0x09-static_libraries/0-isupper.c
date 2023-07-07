3f8573fee048fcd0c04c2b186e689502456f72fc
Nzioka6350 <Nzioka6350>
2023-07-07 11:02:28 -0700

    Nzioka

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a character is an uppercase letter
+ * @c: character to check
+ *
+ * Return: 1 if the character is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
