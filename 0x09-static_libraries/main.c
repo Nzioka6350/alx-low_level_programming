d1e9a96e25e0d0f95a840aeb694148cf66948a6c
Nzioka6350 <Nzioka6350>
2023-07-07 09:36:52 -0700

    I will

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* main - Entry point
+*
+* Description: Prints a quote using the _puts function
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- James Nzioka");
+	return (0);
+}
