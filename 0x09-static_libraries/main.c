58d1f89c9b00173c2fe7adc94a134ebe80fe93c8
Nzioka6350 <Nzioka6350>
2023-07-07 09:41:52 -0700

    I will

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -7,6 +7,6 @@
 */
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- James Nzioka");
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }
