3003657763d761c418712dcf4312dd82b9faf88f
Nzioka6350 <Nzioka6350>
2023-07-19 03:37:47 -0700

    I can

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,15 @@
 #include "function_pointers.h"
 #include <stdio.h>
-
 /**
- * print_with_function - Calls a function to print a name.
- * @name: The string to be printed.
- * @print_function: Pointer to the function responsible for printing the name.
- *
- * Return: Nothing.
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
  **/
-void print_with_function(char *name, void (*print_function)(char *))
+void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || print_function == NULL)
+	if (name == NULL || f == NULL)
 		return;
 
-	print_function(name);
+	f(name);
 }
