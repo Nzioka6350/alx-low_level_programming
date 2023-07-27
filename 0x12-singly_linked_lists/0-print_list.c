287ce78a0f50d7cbd8e41ce81d82a78dd1106db0
Nzioka6350 <jamesnziokamutua1@gmail.com>
2023-07-27 02:59:54 -0700

    Nzioka

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "lists.h"
+
 /**
  * print_list - Prints all the elements of a linked list.
  * @h: Pointer to the list_t list to print.
@@ -16,8 +17,10 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
-			h = h->next;
-			s++;
+
+		h = h->next;
+		s++;
 	}
+
 	return (s);
 }
