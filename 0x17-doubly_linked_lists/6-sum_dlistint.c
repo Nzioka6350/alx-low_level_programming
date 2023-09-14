2a3cd0bd26367c914327ca1117c4538048767fa7
Nzioka6350 <jamesnziokamutua1@gmail.com>
2023-09-14 08:45:38 -0700

    Task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -5,14 +5,21 @@
  * @head: list
  * Return: int
  */
-
 int sum_dlistint(dlistint_t *head)
 {
+	/* Initialize the sum variable */
 	int sum = 0;
+
+	/* Traverse the doubly-linked list until the end (NULL is reached) */
 	while (head != NULL)
 	{
+		/* Add the current node's 'n' value to the sum */
 		sum += head->n;
+
+		/* Move to the next node in the list */
 		head = head->next;
 	}
+
+	/* Return the calculated sum */
 	return (sum);
 }
